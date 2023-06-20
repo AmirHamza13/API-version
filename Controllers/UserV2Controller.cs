cf96a75a19fbd3cd22d9f3e8dfe87b1ce2732913
AmirHamza13 <amirhamza836@gmail.com>
2023-06-20 12:41:48 +0600

    FirstUp

---
+++ Controllers/UserV2Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioningTest.Controllers
+{
+    //Route for Query string parameter versioning
+    //[Route("api/user/[action]")]
+    //Route for URI versioning
+    [Route("api/v{version:apiVersion}/user/[action]")]
+    [ApiController]
+    [ApiVersion("2")]
+    public class UserV2Controller : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetInfo() 
+        {
+            return Ok("User V2 Controller");
+        }
+    }
+}
